777c1e810fe742e02b336e6a7a44f45b518599e1
Damanjotkaur <damanjot1996@gmail.com>
2020-02-18 10:41:05 +0530

    Project1

--- inher.py
+++ inher.py
@@ -33,3 +33,37 @@ h1=B()
 h1.func1()
 h1.func()
 
+# Ass3
+class a():
+    a=20
+    x=50
+    def __init__(self):
+        print("The value of a",self.a)
+class b(a):
+    b=20
+    def func(self):
+        print("The value of b",self.b)
+class c(b):
+    y=30
+    def func1(self):
+         print("The value of y",self.y)
+         print("The value of x",super().x)
+h1=c()
+h1.func1()
+h1.func()
+h1.__init__()
+
+# Ass4
+class Parent:
+    def func1(self):
+        print("this is function 1")
+class Parent2:
+    def func2(self):
+        print("this is function 2")
+class Child(Parent, Parent2):
+    def func3(self):
+        print("this is function 3")
+ob = Child()
+ob.func1()
+ob.func2()
+ob.func3()
